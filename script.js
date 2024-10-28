af59f9cf7f8318f0f610de842bc60af37a6db8a3
bns1998 <iraqiammar98@gmail.com>
2024-10-28 09:45:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+const ordersTableBody = document.getElementById('ordersTableBody');
+const totalPriceWithoutDeliveryElement = document.getElementById('totalPriceWithoutDelivery');
+const totalPriceWithDeliveryElement = document.getElementById('totalPriceWithDelivery');
+let orders = JSON.parse(localStorage.getItem('orders')) || [];
+
+// استرجاع الطلبات المخزنة عند تحميل الصفحة
+window.onload = function() {
+    updateTable();
+};
+
+function isValidInput(orderNumber, totalPrice) {
+    const orderNumberValid = /^[0-9]+$/.test(orderNumber);
+    const totalPriceValid = Number.isInteger(totalPrice) && totalPrice > 0;
+
+    return orderNumberValid && totalPriceValid;
+}
+
+function addOrder(deliveryPrice) {
+    const orderNumber = document.getElementById('orderNumber').value;
+    const totalPrice = parseInt(document.getElementById('totalPrice').value, 10);
+
+    if (!isValidInput(orderNumber, totalPrice)) {
+        alert('يرجى التأكد من إدخال رقم الطلب والسعر الكلي بشكل صحيح.');
+        return;
+    }
+
+    if (orders.some(order => order.number === orderNumber)) {
+        alert('رقم الطلب موجود بالفعل.');
+        return;
+    }
+
+    const priceWithoutDelivery = totalPrice - deliveryPrice;
+    const newOrder = { number: orderNumber, totalPrice, deliveryPrice, priceWithoutDelivery };
+    orders.push(newOrder);
+    localStorage.setItem('orders', JSON.stringify(orders));
+    updateTable();
+    document.getElementById('orderNumber').value = '';
+    document.getElementById('totalPrice').value = '';
+}
+
+function updateTable() {
+    ordersTableBody.innerHTML = '';
+    let totalWithoutDelivery = 0;
+    let totalWithDelivery = 0;
+
+    orders.forEach(order => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${order.number}</td>
+            <td>${order.totalPrice}</td>
+            <td>${order.deliveryPrice}</td>
+            <td>${order.priceWithoutDelivery}</td>
+        `;
+        ordersTableBody.appendChild(row);
+        totalWithoutDelivery += order.priceWithoutDelivery;
+        totalWithDelivery += order.totalPrice;
+    });
+
+    totalPriceWithoutDeliveryElement.textContent = totalWithoutDelivery;
+    totalPriceWithDeliveryElement.textContent = totalWithDelivery;
+}
+
+function clearOrders() {
+    const confirmClear = confirm('هل أنت متأكد أنك تريد مسح السجل؟');
+    if (confirmClear) {
+        orders = [];
+        localStorage.removeItem('orders');
+        updateTable();
+    }
+}
+
+function exportToExcel() {
+    const ws = XLSX.utils.json_to_sheet(orders);
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, "الطلبات");
+    XLSX.writeFile(wb, "الطلبات.xlsx");
+}
